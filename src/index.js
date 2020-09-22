7daea8c87fc670c1a874b62446b96483f7d1ec38
Javier <javier.fuentesm@hotmail.com>
2020-09-22 19:21:04 -0400

    using formik components

--- src/index.js
+++ src/index.js
@@ -1,50 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import "./styles.css";
 import * as Yup from "yup";
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: { email: "", name: "", lastName: "" },
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" {...formik.getFieldProps("name")} />
-
-      {formik.touched.name && formik.errors.name ? (
-        <div style={{ color: "red" }}>{formik.errors.name}</div>
-      ) : null}
-      <label htmlFor="lastName">Last Name</label>
-      <input id="lastName" type="text" {...formik.getFieldProps("lastName")} />
-
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div style={{ color: "red" }}>{formik.errors.lastName}</div>
-      ) : null}
-
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" {...formik.getFieldProps("email")} />
-      <br />
-      {formik.touched.email && formik.errors.email ? (
-        <div style={{ color: "red" }}>{formik.errors.email}</div>
-      ) : null}
-
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={{ firstName: "", lastName: "", email: "" }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" />
+        <ErrorMessage name="firstName" />
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
+        <label htmlFor="email">Email Address</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <br />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
