8dfdb1c0f9a9c71dbfd74c3dd66fc2937c038c9a
Javier <javier.fuentesm@hotmail.com>
2020-09-22 19:40:41 -0400

    Simplify things

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Formik, Field, Form, useField, ErrorMessage } from "formik";
 import "./styles.css";
 import * as Yup from "yup";
 
 const SignupForm = () => {
+  const MyTextInput = ({ label, ...props }) => {
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input> and also replace ErrorMessage entirely.
+    const [field] = useField(props);
+    return (
+      <>
+        <label htmlFor={props.id || props.name}>{label}</label>
+        <Field {...field} {...props} />
+        <ErrorMessage name={field.name} />
+      </>
+    );
+  };
   return (
     <Formik
       initialValues={{ firstName: "", lastName: "", email: "" }}
@@ -25,15 +37,24 @@ const SignupForm = () => {
       }}
     >
       <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" type="text" />
-        <ErrorMessage name="firstName" />
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" type="text" />
-        <ErrorMessage name="lastName" />
-        <label htmlFor="email">Email Address</label>
-        <Field name="email" type="email" />
-        <ErrorMessage name="email" />
+        <MyTextInput
+          label="First Name"
+          name="firstName"
+          type="text"
+          placeholder="Input your name"
+        />
+        <MyTextInput
+          label="Last Name"
+          name="lastName"
+          type="text"
+          placeholder="Input your lastname"
+        />
+        <MyTextInput
+          label="Email Address"
+          name="email"
+          type="email"
+          placeholder="Input your email"
+        />
         <br />
         <button type="submit">Submit</button>
       </Form>
